cc70841509752d7de1e2666c5ab11722e0902f4e
Naumenko Evgenii <evgeniynaumenko2017@gmail.com>
2024-06-20 16:17:46 +0300

    first commit

---
+++ EngFrDictionary.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_09_14_06_2024
+{
+    internal class EngFrDictionary
+    {
+        public Dictionary<string, List<string>> _dict;
+        public EngFrDictionary() 
+        { 
+            _dict = new Dictionary<string, List<string>>();
+        }
+        public void AddNewWord(string word, string transletion)
+        {
+            if (!_dict.ContainsKey(word))
+            {
+                _dict[word] = new List<string>();
+            }
+            _dict[word].Add(transletion);
+        }
+        public void AddNewWord(string word, List<string> transletion)
+        {
+            if (!_dict.ContainsKey(word))
+            {
+                _dict[word] = transletion;
+            }
+        }
+        public void AddTranslation (string word, string transletion)
+        {
+            _dict[word].Add (transletion);
+        }
+        public string Translate(string word)
+        {
+            if (_dict.ContainsKey(word))
+            {
+                string transl = "";
+                foreach (var item in _dict[word])
+                { 
+                    transl +=(item+", ");
+                }
+                return transl;
+            }
+            else
+            {
+                Console.WriteLine("Слово не найдено в словаре.");
+                return "";
+            }
+        }
+        public void DeleteWord(string word)
+        {
+            _dict.Remove(word);
+        }
+        public void DeleteTranslate(string word)
+        {
+            _dict[word].Clear();
+        }
+         public void ChangeTranslation(string word, string newTranslation)
+        {
+            if (_dict.ContainsKey(word))
+            {
+                _dict[word].Clear();
+                _dict[word].Add(newTranslation);
+            }
+        }
+        
+        public static EngFrDictionary operator+(EngFrDictionary dict1, EngFrDictionary dict2)
+        {
+            foreach (var item in dict2._dict)
+            {   
+                dict1.AddNewWord(item.Key, item.Value);
+            }
+            return dict1;
+        }
+        public override string ToString()
+        {
+            Console.WriteLine("English-France dictionary:");
+            foreach (var item in _dict)
+            {
+                Console.WriteLine($"Word: {item.Key}");
+                Console.Write("Translation: ");
+                foreach (var item2 in item.Value)
+                {
+                    Console.Write(item2 + ", ");
+                }
+                Console.WriteLine();
+            }
+            return "";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+
+namespace HW_09_14_06_2024
+{
+//Завдання 2:
+//Створіть додаток «Англо-французький словник», який має
+//зберігати наступну інформацію:
+// слово англійською мовою;
+// варіанти перекладу французькою.
+//Для зберігання інформації використовуйте Dictionary<T>.
+//Додаток має надавати таку функціональність:
+// додати слово і варіанти перекладу;
+// видалити слово;
+// видалити варіанти перекладу;
+// зміна слова;
+// зміна варіанта перекладу;
+// пошук перекладу слова.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EngFrDictionary dict = new EngFrDictionary();
+            dict.AddNewWord("Hello", "Bonjour");
+            dict.AddNewWord("Book", "Livre");
+            dict.AddTranslation("Book", "Réserver");
+            Console.WriteLine(dict);
+            Console.WriteLine("====================");
+            dict.AddNewWord("Match", "Correspondre");
+            dict.AddTranslation("Match", "Allumette");
+            dict.AddTranslation("Match", "Match");
+            Console.WriteLine(dict);
+            Console.WriteLine("====================");
+            dict.ChangeTranslation("Match", "Correspondre");
+            Console.WriteLine(dict);
+            Console.WriteLine("====================");
+            string word = "Hello";
+            Console.WriteLine($"{word} translation: {dict.Translate(word)}");
+            dict.DeleteWord(word);
+            Console.WriteLine(dict);
+            Console.WriteLine("====================");
+            EngFrDictionary dict2 = new EngFrDictionary();
+            dict2.AddNewWord("Fair", "Juste");
+            dict2.AddNewWord("Right", "Droit");
+            dict2.AddNewWord("Left", "Gauche");
+            dict += dict2;
+            Console.WriteLine(dict);
+            Console.WriteLine("====================");
+        }
+    }
+}
